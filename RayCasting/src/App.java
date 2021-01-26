b7b436529b961399a5c97d5153a5f9cd45b0f735
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2021-01-26 15:54:46 +0100

    setup classes for the app

--- RayCasting/src/App.java
+++ RayCasting/src/App.java
@@ -1,5 +1,19 @@
 public class App {
+
+    public App() {
+
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Starting the App");
+
+        App app = new App();
+        app.start();
+
+        System.out.println("End");
+    }
+
+    public void start() {
+
     }
 }
---
+++ RayCasting/src/engine/logic/Point2D.java
@@ -0,0 +1,5 @@
+package engine.logic;
+
+public class Point2D {
+    
+}
